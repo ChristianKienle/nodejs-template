881f18558f785f3f6eaa786d5657ebbb664434a9
Christian Kienle <christian.kienle@sap.com>
2018-08-25 11:30:47 +0200

    added comments to .babelrc.js and fixed an async/await-bug

--- .babelrc.js
+++ .babelrc.js
@@ -1,9 +1,31 @@
-const presets = ['@babel/env', '@babel/preset-flow'];
+const presets = [
+  // If you are not using flow remove the following line.
+  '@babel/preset-flow',
+  ['@babel/env',
+    {
+      // Require the highest node version you can.
+      // You should use at least a node version which 
+      // supports async/await because Babel has been
+      // configured without polyfills/generators for
+      // async/await.
+      targets: { node: '10.0.0' }
+    }
+  ]
+];
+
 const plugins = [];
 
 module.exports = {
   presets,
   plugins,
+
+  // For compatibility we generate inline source maps _and_
+  // source maps in dedicated files. 
   sourceMaps: 'both',
-  retainLines: true
+
+  // Retaining lines increases debugability but may lead to less
+  // readable source code. However if you have to debug for whatever
+  // reason it is better to have the lines match up than to have 
+  // nicer source code.
+  retainLines: true,
 };
